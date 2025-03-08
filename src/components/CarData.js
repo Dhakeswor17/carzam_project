6eba3fcd2f60dde21607783eb8cd516ee24c119a
dhakeswor <santoshneupane17@gmail.com>
2025-03-08 11:01:08 +0200

    making the dummy data for car

---
+++ src/components/CarData.js
@@ -0,0 +1,44 @@
+const carData = [
+    {
+        license_plate: "XYZ-789",
+        vehicle_health: 7,
+        safety_rating: 8,
+        market_value: 22000,
+        common_issues: {
+            "Engine Misfire": "12%",
+            "Oil leak": "9%",
+            "Battery Drain": "6%"
+        },
+        basic_info: {
+            make: "Mazda",
+            model: "CX-5",
+            year: 2020,
+            type: "SUV",
+            fuel: "Gasoline",
+            transmission: "Automatic"
+        },
+        yearly_maintenance_cost: 700
+    },
+    {
+        license_plate: "ABC-123",
+        vehicle_health: 9,
+        safety_rating: 9,
+        market_value: 30000,
+        common_issues: {
+            "Engine Misfire": "5%",
+            "Oil leak": "3%",
+            "Battery Drain": "2%"
+        },
+        basic_info: {
+            make: "Toyota",
+            model: "RAV4",
+            year: 2021,
+            type: "SUV",
+            fuel: "Hybrid",
+            transmission: "Automatic"
+        },
+        yearly_maintenance_cost: 500
+    }
+];
+
+export default carData;
