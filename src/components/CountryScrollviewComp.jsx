7e22b6ec39636b15b203299668dd6e491c9d8e38
dhakeswor <santoshneupane17@gmail.com>
2025-03-08 11:01:52 +0200

    modify

---
+++ src/components/CountryScrollviewComp.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function CountryScrollviewComp({ countries, onCountrySelect }) {
+    return (
+        <div style={{ marginTop: "20px" }}>
+            <h2>Select a Country</h2>
+            <div
+                style={{
+                    maxHeight: "200px", 
+                    overflowY: "scroll", 
+                    border: "1px solid #ccc", 
+                    padding: "10px", 
+                }}
+            >
+                {countries.map((country, index) => (
+                    <p
+                        key={index}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => onCountrySelect(country)}
+                    >
+                        {country}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default CountryScrollviewComp
