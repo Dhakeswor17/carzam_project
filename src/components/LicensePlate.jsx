ed8bbfcc3a6ba658292483519e1ab88e934fd40a
dhakeswor <santoshneupane17@gmail.com>
2025-03-25 22:23:48 +0200

    adding licensePlate components

---
+++ src/components/LicensePlate.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+//https://regcheck.org.uk/api/reg.asmx
+
+
+function LicencePlate({ data }) {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    // Filter the data based on the search query
+    const LicencePlate = data.filter(item =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    return (
+        <div>
+            <input 
+                type="text" 
+                placeholder="Search Licence plate" 
+                value={searchQuery} 
+                onChange={(e) => setSearchQuery(e.target.value)} 
+            />
+            {LicencePlate.map(item => (
+                <p key={item.id}>{item.name}</p>
+            ))}
+        </div>
+    );
+}
+
+export default LicencePlate;
