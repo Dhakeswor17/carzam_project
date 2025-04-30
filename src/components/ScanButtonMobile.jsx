680c054fadc9c74994e16d3753f4db1a8b75ef3b
dhakeswor <Santoshneupane17@gmail.com>
2025-04-30 23:45:46 +0300

    fixing the footer

--- src/components/ScanButtonMobile.jsx
+++ src/components/ScanButtonMobile.jsx
@@ -1,33 +1,64 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useRef } from 'react';
-import './ScanButtonMobile.css';
+import { useRef, useState } from 'react';
+import '../styles/ScanButtonMobile.css';
 import { useNavigate } from 'react-router-dom';
 
 /* eslint-disable react/prop-types */
 function Button({ label }) {
-  
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fileInputRef = useRef(null);
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
 
-  
+
   const handleButtonClick = () => {
+
+
     if (fileInputRef.current) {
       fileInputRef.current.click();
     }
+
+
+
   };
 
   // Handle the selected (or captured) file
-  const handleFileChange = (event) => {
+  const handleFileChange = async (event) => {
     const { files } = event.target;
     if (files && files.length > 0) {
       const file = files[0];
       console.log('Captured file:', file);
-      navigate('/loading')
-      setTimeout(() => {
-        navigate('/')
-      }, 3000);
+  
+      const formData = new FormData();
+      formData.append('image', file);
+  
+      try {
+        const response = await fetch('http://localhost:5000/upload', {
+          method: 'POST',
+          body: formData,
+        });
+  
+        const result = await response.json();
+        const licensePlate = result.parsedResponse.license_plate;
+        const country = result.parsedResponse.license_plate_country;
+  
+        console.log('Response from backend:', licensePlate, country);
+  
       
+        navigate("/loading", {
+          state: {
+            searchQuery: licensePlate,
+            selectedCountry: country
+          }
+        });
+  
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        setError("Failed to upload file");
+      }
     }
   };
 
