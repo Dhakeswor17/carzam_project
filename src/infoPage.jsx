f1825397198f863eeaf193828465ca0310cd7b63
dhakeswor <Santoshneupane17@gmail.com>
2025-05-03 23:26:46 +0300

    fixing the components

--- src/infoPage.jsx
+++ src/infoPage.jsx
@@ -84,7 +84,15 @@ function InfoScreen({ data }) {
 
       
       <footer className="full-width-footer">
-        
+        <div>
+          <p className="mb-2">Good2Know is a Trademark of 123 Oy</p>
+          <div className="footer-links">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Use</a>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
