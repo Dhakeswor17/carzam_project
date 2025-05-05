37e60c15c2ad2a56e6d2cf3dcd57690f8491798e
dhakeswor <Santoshneupane17@gmail.com>
2025-05-05 21:24:07 +0300

    adding mimic data for checking only

--- src/components/CarData.js
+++ src/components/CarData.js
@@ -1,4 +1,4 @@
-const carData = [
+const carDatabase = [
     {
         license_plate: "XYZ-789",
         vehicle_health: 7,
@@ -6,7 +6,7 @@ const carData = [
         market_value: 22000,
         common_issues: {
             "Engine Misfire": "12%",
-            "Oil leak": "9%",
+            "Oil Leak": "9%",
             "Battery Drain": "6%"
         },
         basic_info: {
@@ -18,27 +18,7 @@ const carData = [
             transmission: "Automatic"
         },
         yearly_maintenance_cost: 700
-    },
-    {
-        license_plate: "ABC-123",
-        vehicle_health: 9,
-        safety_rating: 9,
-        market_value: 30000,
-        common_issues: {
-            "Engine Misfire": "5%",
-            "Oil leak": "3%",
-            "Battery Drain": "2%"
-        },
-        basic_info: {
-            make: "Toyota",
-            model: "RAV4",
-            year: 2021,
-            type: "SUV",
-            fuel: "Hybrid",
-            transmission: "Automatic"
-        },
-        yearly_maintenance_cost: 500
     }
 ];
 
-export default carData;
+export default carDatabase;
