f43afdfdd873dd695e58998ca99a90c36931b7eb
dhakeswor <Santoshneupane17@gmail.com>
2025-05-06 21:36:57 +0300

    chnage the functionalities

--- src/components/CheckListComp.jsx
+++ src/components/CheckListComp.jsx
@@ -1,48 +1,35 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 
-function CheckListComp({ checks, isLicensePlateFound }) {
+function CheckListComp({ checks }) {
   return (
     <div className="col flex-column">
       <div className="col d-flex justify-content">
-        {isLicensePlateFound ? (
-          <span style={{ color: "green", fontSize: "1.5rem" }}>✓</span>
-        ) : (
-          <Spinner
-            className="mt-2 me-3"
-            animation="grow"
-            size="sm"
-            style={{ color: checks[0] ? "green" : "red" }}
-          />
-        )}
+        <Spinner
+          className="mt-2 me-3"
+          animation="grow"
+          size="sm"
+          style={{ color: checks[0] ? "green" : "red" }}
+        />
         <p>Getting data from...</p>
       </div>
       <div className="col d-flex justify-content">
-        {isLicensePlateFound ? (
-          <span style={{ color: "green", fontSize: "1.5rem" }}>✓</span>
-        ) : (
-          <Spinner
-            className="mt-2 me-3"
-            animation="grow"
-            size="sm"
-            style={{ color: checks[1] ? "green" : "red" }}
-          />
-        )}
+        <Spinner
+          className="mt-2 me-3"
+          animation="grow"
+          size="sm"
+          style={{ color: checks[1] ? "green" : "red" }}
+        />
         <p>Processing....</p>
       </div>
       <div className="col d-flex justify-content">
-        {isLicensePlateFound ? (
-          <span style={{ color: "green", fontSize: "1.5rem" }}>✓</span>
-        ) : (
-          <Spinner
-            className="mt-2 me-3"
-            animation="grow"
-            size="sm"
-            style={{ color: checks[2] ? "green" : "red" }}
-          />
-        )}
+        <Spinner
+          className="mt-2 me-3"
+          animation="grow"
+          size="sm"
+          style={{ color: checks[2] ? "green" : "red" }}
+        />
         <p>Processing....</p>
       </div>
     </div>
--- src/components/issueBlob.jsx
+++ src/components/issueBlob.jsx
@@ -1,13 +1,24 @@
-//import { useRef } from 'react';
+import { useRef } from 'react';
 import carData from './mimicdata.jsx';
 import './issueBlob.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function IssueBlob() {
-  const issues = carData.common_issues;
+function IssueBlob({issues}) {
+  
+  const datas = issues
 
-  function getBadgeColor() {
+  function getBadgeColor(value) {
+    console.log("value", value);
+    if (value >= 70) return 'bg-danger'; // High severity (red)
+    if (value >= 30) return 'bg-warning'; // Medium severity (yellow)
     return 'bg-success'; // Low severity (green)
+  }
+
+  function getStatus(value) {
+    console.log("value", value);
+    if (value >= 70) return 'Very likely'; // High severity (red)
+    if (value >= 30) return 'Middle ground'; // Medium severity (yellow)
+    return 'Not likely'; // Low severity (green)
   }
 
   function ListOfIssues() {
@@ -16,9 +27,9 @@ function IssueBlob() {
         <h5 className="mb-3 text-center">Common Issues</h5>
         <div className="card_issues shadow-lg  bg-dark text-white rounded d-flex justify-content-center align-items-center text-white">
           <ul className='mt-3'>
-            {Object.keys(issues).map((issue) => (
-              <li key={issue} className="list-group-item mb-2">
-                {issue} :  <span className={`badge ${getBadgeColor(issues[issue])}`}>{issues[issue]}</span>
+            {Object.keys(datas).map((data) => (
+              <li key={data} className="list-group-item mb-2">
+                {data} :  <span className={`badge ${getBadgeColor(datas[data])}`}>{getStatus(datas[data])}</span>
               </li>
             ))}
           </ul>
@@ -27,7 +38,7 @@ function IssueBlob() {
     );
   }
 
-  console.log("lol", issues);
+
 
   return (
     <>
