e9c664c9859b125f72b7a7580f67867d076f5fbc
dhakeswor17 <santoshneupane17@gmail.com>
2025-11-03 22:51:19 +0200

    fixing the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScanButton from './components/ScanButtonMobile.jsx';
 import './App.css';
 import DataFetching from './components/ApiData';
